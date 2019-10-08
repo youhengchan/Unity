306b7954a3634acfeda9af0dfccfb331040264ad
youhengchan <youhengchan@qq.com>
2019-10-08 12:54:39 +0800

    Added Solar system Asserts

---
+++ SolarSystemAssets/Scripts/Satellite.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Satellite : StarBase
+{
+    public GameObject rotateCenterObject;
+    public Vector3 pubRotateNormalAngle;
+    public float pubRotateAngularVelocity;
+    void Start()
+    {
+        // Initialize the Self Rotation Tilt Angle first
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Implement Self Rotation
+        base.Update();
+        // Spin the object around the world origin at pubRotateAngularVelocity degrees/second.
+        star.transform.RotateAround(rotateCenterObject.transform.position, pubRotateNormalAngle, pubRotateAngularVelocity * Time.deltaTime);
+    }
+}
---
+++ SolarSystemAssets/Scripts/StarBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarBase : MonoBehaviour
+{
+    public GameObject star;
+    public Vector3 selfRotateAngularVelocity;
+    public Vector3 selfRotateTiltAngle;
+    protected void Start()
+    {
+        // Initialize the Rotation of star refering to the world cordinates
+        star.transform.eulerAngles = selfRotateTiltAngle;
+        // For parental transformation : 
+        // transform.localEulerAngles = new Vector3(arg1,arg2,arg3);
+    }
+    protected void SelfRotate()
+    {
+        star.transform.Rotate(selfRotateAngularVelocity, Space.Self);
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        SelfRotate();
+        // Do nothing
+    }
+}
